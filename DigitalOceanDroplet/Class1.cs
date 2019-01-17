15fabfa241eb375a6798f7ab87600c315abb3f40
kwmaddox <kendall.maddox@gmail.com>
2019-01-17 00:09:30 -0600

    Add project files.

---
+++ DigitalOceanDroplet/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalOcean
+{
+    public class Droplet
+    {
+        public string name;
+        public string size;
+        public string region;
+        public string image;
+
+        public Droplet create()
+        {
+            return new Droplet();
+        }
+
+        public Droplet(string name, string region, string size, string image)
+        {
+            this.name = name;
+            this.size = size;
+            this.region = region;
+            this.image = image;
+        }
+
+        public Droplet() : this("Placeholder Name", "nyc1", "s-1vcpu-1gb", "ubuntu-16-04-x64") {
+        }
+    }
+
+    
+
+}
---
+++ DigitalOceanDropletTests/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using NUnit.Framework;
+using DigitalOcean;
+
+namespace DigitalOceanDropletTests
+{
+    [TestFixture]
+    public class CreateDroplet
+    {
+        private Droplet droplet;
+        [SetUp]
+        public void Init()
+        {
+            droplet = new Droplet();
+        }
+
+        [Test]
+        public void ReturnsADroplet()
+        {
+            var result = droplet.create();
+            Assert.IsInstanceOf<Droplet>(result);
+        }
+
+        [Test]
+        public void ConstructsDropletWithGivenValues()
+        {
+            droplet = new Droplet(
+                name:"testName",
+                region:"testRegion",
+                size:"testSize",
+                image:"testImage"   
+            );
+
+
+            Assert.AreEqual(droplet.name, "testName");
+
+            Assert.AreEqual(droplet.region, "testRegion");
+            Assert.AreEqual(droplet.size, "testSize");
+            Assert.AreEqual(droplet.image, "testImage");
+
+
+        }
+
+        [Test]
+        public void DefaultConstructorSetsDefaultValues()
+        {
+            Assert.AreEqual(droplet.name, "Placeholder Name");
+            Assert.AreEqual(droplet.region, "nyc1");
+            Assert.AreEqual(droplet.size, "s-1vcpu-1gb");
+            Assert.AreEqual(droplet.image, "ubuntu-16-04-x64");
+        }
+    }
+}
